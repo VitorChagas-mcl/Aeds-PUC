0aac69787689f87cc3836e82a24fbea7d045fa32
Vitor Chagas <vitorcjagas@gmail.com>
2025-09-01 20:28:52 -0300

    Update L3A6.c

--- listas/lista3/L3A6.c
+++ listas/lista3/L3A6.c
@@ -3,18 +3,24 @@
 César. Nesse caso, cada caractere da mensagem deve ser substituído pelo caractere cujo código
 ASCII é igual ao seu mais uma constante K (lida do teclado).
 */
-
+//Ainda estou tentando fazer ela, mas esta ai pronta
 #include <stdio.h>
 
 int main(){
     char mensagem[100];
+    int k;
     printf("Digite sua mesagem: ");
     fgets(mensagem, sizeof(mensagem), stdin);
+    printf("Digite o valor da constante: \n");
+    scanf("%d", &k);
+
     for(int i = 0; mensagem[i] != '\0'; i++){
-        if(mensagem[i] >= 'a' && mensagem[i] <= 'z'){
-            mensagem[i] += 3;
+        if( mensagem[i] == 'x' || mensagem[i] == 'X' ||mensagem[i] == 'y' || mensagem[i] == 'z' || mensagem[i] == 'Y' || mensagem[i] == 'Z'){
+            mensagem[i] -= 23;
+        }else if(mensagem[i] >= 'a' && mensagem[i] <= 'z'){
+            mensagem[i] +=k;
         }else if(mensagem[i] >= 'A' && mensagem[i] <= 'Z'){
-            mensagem[i] += 3;
+            mensagem[i] +=k;
         }
     }
     for(int i = 0; mensagem[i] != '\0'; i++){
